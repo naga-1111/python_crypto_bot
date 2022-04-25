ab39b3d4fb12bb0a0ece9f7b9395f81ec6821121
nagafune-bit <104352450+nagafune-bit@users.noreply.github.com>
2022-04-25 23:49:48 +0900

    Update configm.py

--- configm.py
+++ configm.py
@@ -9,7 +9,7 @@ class Configm(object):
                 "secret":"",
                 "option":{"defaultType":"future"}})
         ##################################トレード用基本パラメータ
-        self.bot_path = "/home/ec2-user/binance_3bots"  #このコードをおいてるパス、ポジションCSVとかここに保存される
+        self.bot_path = ""  #このコードをおいてるパス、ポジションCSVとかここに保存される
         #self.bot_path = ""
         
         self.mirror_name1 = "*"             #
@@ -18,25 +18,25 @@ class Configm(object):
         self.mirror_lot2 = 0.0004           #
         self.dbg = "OFF"                    #デバッグモード実際に注文しないならON、本稼働ならOFF
         self.leverage = 20                  #レバレッジ設定
-        self.coinm = False                   #coinmモード、Trueならcoinmやる、Falseならcoinmやらない        
+        self.coinm = False                  #
         
-        ###クロスセクションのbot
-        self.bybitls_usd = 4000              #クロスセクションbybitlsロジックbotで1銘柄に何ドル持つか
-        self.ls_time = "16:00:04"              #自分の環境の時刻で何時にlsのポジションチェックをするか（UTC16時が良い？）crontabで動かすなら関係ない
-        self.symboln = 10                   #片側何銘柄ずつポジションを持つか
+        ###
+        self.bybitls_usd = 4000             #
+        self.ls_time = "16:00:04"           #
+        self.symboln = 10                   #
         self.rebal_ls_cross = "OFF"         #リバランスON/OFF（基本はOFFで稼働しっぱなしで、ロット上げ下げするときは、一回トレード完了するまでONにしておく）
 
-        ###タイムシリーズのbot
-        self.ls_diff_usd = 32000              #タイムシリーズls_diff、現先乖離のパラメータ、何ドルポジションを持つか
-        self.long_only="OFF"                #ONにするとロングしかしないbotになる
-        self.trade_time = 15                #JST1時、UTC16時にトレードするなら16-1=15を入れる（自分のサーバの時刻がJSTでも世界標準時で入れる）
-        self.rebal_ls_timeseri = "OFF"      #リバランスON/OFF（基本はOFFで稼働しっぱなしで、ロット上げ下げするときは、一回トレード完了するまでONにしておく）
+        ###
+        self.ls_diff_usd = 32000            #
+        self.long_only="OFF"                #
+        self.trade_time = 15                #
+        self.rebal_ls_timeseri = "OFF"      #
         
-        ###有料noteのbot
-        self.cci_usd = 500                   #cciロジックbotパラメータ
+        ###
+        self.cci_usd = 500                  #
         
-        ###価格偏差値のbot
-        self.usd = 100                      #価格偏差値botパラメータ、ミラトレには関係無し     1銘柄に何ドル使うか、状況に応じて0~5銘柄注文する
+        ###
+        self.usd = 100                      #
         
         
         ##################################SQL通信用、Discord用パラメータ
